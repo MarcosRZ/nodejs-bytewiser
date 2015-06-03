6f162f90af7c688ea845212aa24de43ea62a1245
MarcosRZ <msucaro@gmail.com>
2015-06-03 22:52:22 +0200

    Last challenge completed!

--- 07_array_buffers.js
+++ 07_array_buffers.js
@@ -1,2 +1,4 @@
-var a32 = new Uint32Array([process.argv[2]])
-console.log(JSON.stringify(new Uint16Array(a32)))
+var a32 = new Uint32Array(1)
+a32[0] = process.argv[2]
+var a16 = new Uint16Array(a32.buffer)
+console.log(JSON.stringify(a16))
